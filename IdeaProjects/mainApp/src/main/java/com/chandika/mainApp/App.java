89b1f2cb3522f02dbd1b0e54a058afa6d33588a7
nchandika <nadee.chandika@gmail.com>
2017-04-25 11:07:13 +0530

    first commit

---
+++ IdeaProjects/mainApp/src/main/java/com/chandika/mainApp/App.java
@@ -0,0 +1,16 @@
+package com.chandika.mainApp;
+
+import com.chandika.module1.Module;
+
+import java.io.PrintStream;
+
+/**
+ * Created by chandika on 4/21/17.
+ */
+public class App {
+
+    public static void main(String[] args){
+        System.out.println("Hello from mainApp");
+        System.out.println(Module.getText());
+    }
+}
---
+++ IdeaProjects/module1/src/main/java/com/chandika/module1/Module.java
@@ -0,0 +1,10 @@
+package com.chandika.module1;
+
+/**
+ * Created by chandika on 4/21/17.
+ */
+public class Module {
+    public static String getText(){
+        return "Hello from module one";
+    }
+}
